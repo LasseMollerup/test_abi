823a0da09635c38308aca2194aed7ac373fdf0aa
LasseMollerup <l.mollerup@hotmail.com>
2020-03-05 22:53:42 +0100

    tried to implement GraphQL, however there are something I do not know how to implement. Will go with axios instead

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './App.css';
 
-import Header from './header'
+// Components
+import Header from './header';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='body'>
-        <Header/>
-      </div>
+// ApolloProvider
+import { ApolloProvider } from 'react-apollo';
+
+// ApolloCLient
+import ApolloCLient from 'apollo-boost';
+
+const client = new ApolloCLient({
+  uri: 'https://anapioficeandfire.com/api/'
+})
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>Hello There</h2>
     </div>
-  );
-}
+  </ApolloProvider>
+)
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className='body'>
+//         <Header/>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
